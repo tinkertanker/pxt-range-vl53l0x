746adc098b4e6f47435669ef42d0038f23fe58c2
Sam Gao <gaoshan_274@hotmail.com>
2018-05-11 17:53:35 +0100

    not sure what's going on here

--- distance.cpp
+++ distance.cpp
@@ -2,19 +2,17 @@
 #include "VL53L0X.h"
 
 namespace Rangefinder {
-	I2C i2c(I2C_SDA0, I2C_SCL0);
-	Timer t;
+	I2C *i2c;
+	Timer *t;
 	VL53L0X *ranger;
 
 	//%
 	void init(){
-		ranger = new VL53L0X(&i2c, &t);
+		i2c = new I2C(I2C_SDA0, I2C_SCL0);
+		t = new Timer();
+		ranger = new VL53L0X(i2c, t);
 		ranger->init();
 		ranger->setTimeout(500);
-		ranger->setSignalRateLimit(0.1);
-	  	// increase laser pulse periods (defaults are 14 and 10 PCLKs)
-	  	ranger->setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
-	  	ranger->setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
 	}
 
 	//%
